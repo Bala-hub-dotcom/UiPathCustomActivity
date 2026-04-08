9778561ddee7d967eb0ff3bcf05c7ea6b8e1e9eb
Balachandrasekhar Chaluvadi <balachandrasekharc@gmail.com>
2026-04-08 10:22:07 +0000

    Add UiPath custom activity project

---
+++ PrtMessageActivity/PrtMessageActivity.vb
@@ -0,0 +1,14 @@
+﻿Imports System
+Imports System.Activities
+
+Public Class PrtMessageActivity
+    Inherits CodeActivity
+
+    Public Property Name As InArgument(Of String)
+    Public Property Result As OutArgument(Of String)
+
+    Protected Overrides Sub Execute(context As CodeActivityContext)
+        Dim inputName = Name.Get(context)
+        Result.Set(context, "Hello, " & inputName)
+    End Sub
+End Class
